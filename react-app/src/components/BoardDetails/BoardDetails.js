ab54189d3106b69d834a44be66fecb2c13b74662
Hanan Jomaa <hanan91jomaa@gmail.com>
2023-06-03 11:48:49 -0700

    styling boards detail page

--- react-app/src/components/BoardDetails/BoardDetails.js
+++ react-app/src/components/BoardDetails/BoardDetails.js
@@ -1,21 +1,32 @@
 import { useParams } from "react-router-dom";
 import { getBoardDetails } from "../../store/board";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import "./BoardDetails.css";
+import { getAllBoards } from "../../store/board";
+import { useModal } from "../../context/Modal";
+import AddToBoardOptionsModal from "../AddToBoardOptionsModal/AddToBoardOptionsModal";
 // import { deleteBoard } from "../../store/board";
 // import { useModal } from "../../context/Modal";
 
 const BoardDetails = () => {
+  // const { setModalContent } = useModal();
   const history = useHistory();
   const dispatch = useDispatch();
+  // const [showModal, setShowModal] = useState(false);
   const { userId, id } = useParams();
   // const { setModalContent } = useModal();
   console.log(userId, id, "USERID");
-
+  // const sessionUser = useSelector((state) => {
+  //   return state?.session?.user;
+  // });
   //   const user = useSelector((state) => {
   //     return state.session.user.id;
   //   });
+  // const user = useSelector((state) => {
+  //   return state?.session?.user?.id;
+  // });
 
   const board = useSelector((state) => {
     return state?.board.details;
@@ -23,7 +34,22 @@ const BoardDetails = () => {
   console.log(board, "BOARD DETAIL STATE");
   useEffect(() => {
     dispatch(getBoardDetails(userId, id));
+    // if (user) {
+    //   dispatch(getAllBoards(user));
+    // }
   }, [dispatch, userId, id]);
+
+  // const openModal = () => {
+  //   setShowModal(true);
+  // };
+
+  // const handlePinClick = (pin) => {
+  //   console.log(pin, "pin selected button *****");
+  //   // setSelectedPin(pin);
+  //   // console.log(selectedPin, "SELECTED PIN LANDING PAGE***");
+  //   setModalContent(<AddToBoardOptionsModal pin={pin} user={user} />);
+  //   openModal();
+  // };
 
   // const handleDeleteBoard = () => {
   //   setModalContent(<DeleteBoardDetailModal />);
@@ -31,28 +57,72 @@ const BoardDetails = () => {
   //   history.push(`/${userId}/boards`);
   // };
   return (
-    <div>
-      {board && board?.pins && board?.id ? (
-        <div>
-          <p>{board?.name}</p>
-          <p>{board?.description}</p>
-          {board?.pins?.map((pin) => {
-            return (
-              <div>
-                <img
-                  src={pin && pin?.images && pin?.images[0]?.image_url}
-                  style={{ width: "450px", height: "400px" }}
-                />
-                {/* <p>{pin?.images[0]?.image_url}</p> */}
-                <p>{pin?.name}</p>
+    <div className="container-for-container">
+      <div className="board-details-container">
+        {board && board?.pins && board?.id ? (
+          <div>
+            <div className="header-board-details">
+              <div className="board-details-name">
+                <p>{board?.name}</p>
               </div>
-            );
-          })}
-          {/* <button onClick={() => handleDeleteBoard()}>Delete Board</button> */}
-        </div>
-      ) : (
-        "Page not Found"
-      )}
+              <div className="image-profile-2">
+                <img
+                  className="profile-image-board"
+                  style={{
+                    width: "60px",
+                    clipPath: "circle(41%)",
+                  }}
+                  src={
+                    "https://res.cloudinary.com/dwphwqyrn/image/upload/v1685589599/16147095-2CE2-4AA6-AA9E-D2F1B47D7CE6_qpx5kd.png"
+                  }
+                />
+              </div>
+              <div className="board-details-description">
+                <p>{board?.description}</p>
+              </div>
+            </div>
+            <div className="pins-container-board-details">
+              {board?.pins?.map((pin) => {
+                return (
+                  <div className="pins">
+                    {/* {sessionUser ? (
+                    <div className="pin-to-board">
+                      <button
+                        className="pin-to-board-button"
+                        onClick={() => handlePinClick(pin)}
+                      >
+                        Save
+                      </button>
+                    </div>
+                  ) : (
+                    <br />
+                  )} */}
+                    <div className="grid-wrapper-2">
+                      <div className="board-details-photos">
+                        <div className="pin-photos">
+                          <img
+                            className="pin-images-2"
+                            src={
+                              pin && pin?.images && pin?.images[0]?.image_url
+                            }
+                            // style={{ width: "450px", height: "400px" }}
+                          />
+                        </div>
+                      </div>
+
+                      {/* <p>{pin?.images[0]?.image_url}</p> */}
+                      <div className="pin-name-landing-page">{pin?.name}</div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            {/* <button onClick={() => handleDeleteBoard()}>Delete Board</button> */}
+          </div>
+        ) : (
+          "Page not Found"
+        )}
+      </div>
     </div>
   );
 };
