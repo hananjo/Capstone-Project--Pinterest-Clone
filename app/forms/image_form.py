2a799e4347b1e36b95066ece542dc7e255b2fd8d
Hanan Jomaa <hanan91jomaa@gmail.com>
2023-06-07 22:11:28 -0700

    aws comment out

--- app/forms/image_form.py
+++ app/forms/image_form.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
-from flask_wtf.file import FileField, FileAllowed, FileRequired
-from app.aws import ALLOWED_EXTENSIONS
+# from flask_wtf.file import FileField, FileAllowed, FileRequired
+# from app.aws import ALLOWED_EXTENSIONS
 
 class ImageForm(FlaskForm):
-    image_url = FileField('Image File', validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
-    # image_url = StringField("Image", validators=[DataRequired()])
+    # image_url = FileField('Image File', validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+    image_url = StringField("Image", validators=[DataRequired()])
     pin_id = IntegerField("Pin_id", validators=[DataRequired()])
     size = StringField('Size', validators=[DataRequired()])
     submit = SubmitField('Submit')
