ec79d9d2548e637c9b80c4cd1c9b56dd90a1d58c
Hanan Jomaa <hanan91jomaa@gmail.com>
2023-07-12 10:31:16 -0700

    restored dev.db

--- react-app/src/components/SearchResults/SearchResults.js
+++ react-app/src/components/SearchResults/SearchResults.js
@@ -77,7 +77,7 @@ const SearchResults = () => {
                 </div>
               );
             })}
-            ;
+            <div style={{ color: "white" }}>;</div>
           </div>
         </div>
       ) : (
